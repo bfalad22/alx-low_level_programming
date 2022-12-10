cdfb8aded88e498bdfe5ed181d3e23d1b40dc347
Bolanle <bolasaliu93@gmail.com>
2022-12-09 16:12:05 -0800

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,21 +8,17 @@
 int main(void)
 {
 	char c = 'a';
-
 	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
-
 	c = 'A';
-
 	while (c <= 'Z')
 	{
 		putchar(c);
 		c++;
-	}
-	
+	}	
 	putchar('\n');
 	return (0);
 }
