da6b8dd4e01972e9e688f087e4ecc3fec0bc40f9
Bolanle <bolasaliu93@gmail.com>
2022-12-12 14:15:53 -0800

    Print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,15 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
-		printf("%d", num);
-
-	printf("\n");
-
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num != '9')
+		{
+		putchar (',');
+		putchar (' ');
+		}
+	}
+	putchar('\n');
 	return (0);
 }
