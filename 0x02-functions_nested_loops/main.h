131dac531f6e0ed84f79fd0c6b3f36bd20858299
Bolanle <bolasaliu93@gmail.com>
2022-12-13 19:38:34 -0800

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c)
 void print_alphabet(void);
@@ -13,6 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
 
 #endif
