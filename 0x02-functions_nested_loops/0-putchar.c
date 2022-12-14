235da6d3796a8231cd6aa39df4f78ad41dcf1c1f
Bolanle <bolasaliu93@gmail.com>
2022-12-13 19:51:32 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - prints _putchar, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
@@ -17,6 +17,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
