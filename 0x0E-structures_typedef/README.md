87402b619f255d174d35492affbf9ae8bb966b27
bfalad22 <bolasaliu93@gmail.com>
2023-01-16 01:10:46 -0800

    Task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,12 @@
+Define a new type struct dog with the following elements:
+name, type = char * age, type = float owner, type = char *
+
+Write a function that initialize a variable of type struct dog
+
+Write a function that prints a struct dog
+
+Define a new type dog.t as a new name for the type struct dog.
+
+Write a function that creates a new dog.
+
+Write a function that frees dogs.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - Dog attributes
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.main - write a new type struct dog
+ *
+ * Return: Always 0
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for dog structure
+ */
+typedef struct dog dog_t;
+
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
